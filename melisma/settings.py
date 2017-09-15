d0e64f3a8bb531c025630fcbbc1406f110682dad
Aishwarya A N <aishwarya.an95@gmail.com>
2017-09-15 16:36:47 +0530

    Create basic app named album
    This app will render the album page.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- melisma/settings.py
+++ melisma/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'album.apps.AlbumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
