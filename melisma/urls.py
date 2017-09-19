a1ac46708078c2d28536d62374819428b93765cf
Aishwarya A N <aishwarya.an95@gmail.com>
2017-09-19 19:34:40 +0530

    Create templates and view needed to display the home page.
    The home page has a basic functionality of displaying a list of albums queried from the database.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- melisma/urls.py
+++ melisma/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from home import views
 
 urlpatterns = [
+    url(r'^$', views.show_home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
